5f1f763175731e7110619137ddc9fcbcc2eb46f7
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-10-09 11:38:01 +0100

    Update FlushEventsCommand.php

--- Console/Command/FlushEventsCommand.php
+++ Console/Command/FlushEventsCommand.php
@@ -1,10 +1,13 @@
 <?php
 declare(strict_types=1);
+
 namespace Merlin\IntrusionDetection\Console\Command;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Magento\Framework\App\ResourceConnection;
+
 class FlushEventsCommand extends Command {
     private $rc;
     public function __construct(ResourceConnection $rc){ $this->rc=$rc; parent::__construct(); }

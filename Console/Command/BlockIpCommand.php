fbe767fee17650966389c06c9f86e149d559c6cb
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-10-09 11:37:28 +0100

    Update BlockIpCommand.php

--- Console/Command/BlockIpCommand.php
+++ Console/Command/BlockIpCommand.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 namespace Merlin\IntrusionDetection\Console\Command;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -17,6 +18,7 @@ class BlockIpCommand extends Command {
             ->addArgument('minutes', InputArgument::OPTIONAL, 'Duration minutes', '60');
         parent::configure();
     }
+
     protected function execute(InputInterface $in, OutputInterface $out): int {
         $ip=(string)$in->getArgument('ip'); $minutes=(int)$in->getArgument('minutes');
         $this->svc->block($ip, 'CLI', $minutes); $out->writeln("<info>Blocked {$ip} for {$minutes} minutes.</info>");

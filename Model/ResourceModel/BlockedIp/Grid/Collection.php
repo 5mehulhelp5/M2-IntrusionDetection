c2465b6aad1b08e14fbe84e9f20536487474c01a
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2025-10-16 14:40:01 +0100

    Update Collection.php

--- Model/ResourceModel/BlockedIp/Grid/Collection.php
+++ Model/ResourceModel/BlockedIp/Grid/Collection.php
@@ -3,19 +3,38 @@ declare(strict_types=1);
 
 namespace Merlin\IntrusionDetection\Model\ResourceModel\BlockedIp\Grid;
 
-use Magento\Framework\View\Element\UiComponent\DataProvider\Document;
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface;
+use Magento\Framework\Data\Collection\EntityFactoryInterface;
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
-use Merlin\IntrusionDetection\Model\ResourceModel\BlockedIp as BlockedIpResource;
+use Psr\Log\LoggerInterface;
 
 class Collection extends SearchResult
 {
-    protected function _construct()
-    {
-        $this->_init(Document::class, BlockedIpResource::class);
-        $this->setMainTable('merlin_blocked_ip');
+    protected $_idFieldName = 'block_id';
 
+    public function __construct(
+        EntityFactoryInterface $entityFactory,
+        LoggerInterface $logger,
+        FetchStrategyInterface $fetchStrategy,
+        ManagerInterface $eventManager,
+        $mainTable = 'merlin_blocked_ip',
+        $resourceModel = \Merlin\IntrusionDetection\Model\ResourceModel\BlockedIp::class
+    ) {
+        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $mainTable, $resourceModel);
+    }
+
+    protected function _initSelect()
+    {
+        parent::_initSelect();
+        
+        // Add filter maps for better column handling
+        $this->addFilterToMap('block_id', 'main_table.block_id');
         $this->addFilterToMap('ip', 'main_table.ip');
         $this->addFilterToMap('created_at', 'main_table.created_at');
         $this->addFilterToMap('expires_at', 'main_table.expires_at');
+        
+        return $this;
     }
 }
